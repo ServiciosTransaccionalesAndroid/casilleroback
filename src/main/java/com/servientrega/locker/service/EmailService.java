922d9b8135ff6f14f5f0dc4ce3b2eac9a7c79ee5
Servientrega Apps <aplicaciones.serviciostrans@gmail.com>
2026-03-04 11:06:54 -0400

    Fix missing @Value import in EmailService

--- src/main/java/com/servientrega/locker/service/EmailService.java
+++ src/main/java/com/servientrega/locker/service/EmailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
